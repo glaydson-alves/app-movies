b4899dc8c836724b9e114c35e1d2e53c63543a2b
glaydson-alves <glaydson.alves36@gmail.com>
2024-01-07 01:34:43 -0300

    ...

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import styles from './Footer.module.css'
+
+function Footer(){
+    return(
+        <footer className={styles.footer}>
+            <small>App Movies &copy; desenvolvido por Glaydson Alves 2024</small>
+        </footer>
+    )
+}
+
+export default Footer
