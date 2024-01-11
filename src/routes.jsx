3b2ec98e96dbaeb874a43105416e5c56c8d5aa96
glaydson-alves <glaydson.alves36@gmail.com>
2024-01-10 21:24:55 -0300

    add page NotFound, iniciado rota para pesquisa

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,15 +1,23 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Page/Home/Home";
 import Watch from "./Page/Watch/watch";
+import PageNotFound from "./Page/PageNotFound";
+import Search from "./Page/Search/Search";
 
 function AppRoutes(){
     return(
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={ <Home /> }></Route>
-                <Route path="/watch" element={ <Watch /> }></Route>
+                <Route path="/watch/:id" element={ <Watch /> }></Route>
+                <Route path="/search" element={ <Search /> }></Route>
+                <Route path="*" element={ <PageNotFound/> }></Route>
+                
                 {/* estou passando duas Props
                 path onde eu estou definindo o ponto de partida/ raiz e o element onde eu defindo onde quero chegar */}
+                {/* path="*" siginifica que se eu acessar qualquer coisa diferente das duas rotas que eu ja defini ele vai para page not found */}
+
+                {/* path="/watch/:id"  estou passando um parametro de id para a rota */}
             </Routes>
         </BrowserRouter>
     )
