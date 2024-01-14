8dde79938b00f0a9dc62dddbf0ebe6af291bd137
glaydson-alves <glaydson.alves36@gmail.com>
2024-01-14 01:21:17 -0300

    ...

--- src/Components/VideoList/VideoList.jsx
+++ src/Components/VideoList/VideoList.jsx
@@ -1,27 +1,24 @@
-import Card from "../Card/Card"
-import styles from "./VideoList.module.css"
+import styles from "./VideoList.module.css";
+import Card from "../Card/Card";
 
-function VideoList({videos, emptyHeading}) {
-
-  const count = videos.length
-  let heading = emptyHeading
-  if(count > 0) {
-    const noun = count > 1 ? "vídeos" : "vídeo"
-    heading = `${count} ${noun}`
+function VideoList({ videos, emptyHeading }) {
+    const count = videos.length
+    let heading = emptyHeading
+    if(count > 0) {
+        const noun = count > 1 ? 'vídeos' : 'vídeo'
+        heading = `${count} ${noun}`
     // nessa função eu faço a verificação de quantidades de videos encontradas pela pesquisa.
     // count vai resceber a quantide encontrada enquato que 'noun' vai verificar se é maior que 1 para ele receber o valor no plural ou no singular.
   }
   //
   return (
     <>
-      <h2>{heading}</h2>
-      <div className={styles.videos}>
-        {videos.map((video) => (
-          <Card id={video.id} key={video.id} />
-        ))}
-      </div>
+        <h2>{heading}</h2>
+        <section className={styles.videos}>
+            { videos.map((video) => <Card id={video.id} key={video.id} /> )}
+        </section>
     </>
-  );
+);
 }
 
-export default VideoList
+export default VideoList;
